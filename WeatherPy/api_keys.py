224e88c9704e1fd2708181e15a672d6bf38f9966
Shay O'Connell <shay.oconnell7@gmail.com>
2020-10-07 16:07:30 -0400

    generated list of cities, created data frame fror city info

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -3,3 +3,10 @@ weather_api_key = "YOUR KEY HERE!"
 
 # Google API Key
 g_key = "YOUR KEY HERE!"
+
+
+# # OpenWeatherMap API Key
+# weather_api_key = "AQ1Os281YyDCO4ubCA2JxWkALwNuvPwU"
+
+# # Google API Key
+# g_key = "AIzaSyAejLSi1XMN_jWXAQn_4jVQGqTZs6M8Wyc"
