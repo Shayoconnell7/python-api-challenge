6c79621ed290ff8bf61f50b01804eafd31672d29
Shay O'Connell <shay.oconnell7@gmail.com>
2020-10-12 18:28:36 -0400

    updated gitignore and api keys

--- VacationPy/api_keys.py
+++ VacationPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "73163d9c184f32a1da0217d72daea743"
+weather_api_key = "0e8be9d2a3a658529133aa0cfe9e80c4"
 
 # Google API Key
-g_key = "AIzaSyAejLSi1XMN_jWXAQn_4jVQGqTZs6M8Wyc"
+g_key = "AIzaSyA7PzEGATFCc9OI3JbUjzJeigQU4ug1uho"
